6cc8180d604679fae47cb11ab134fa97e4576d3e
PC groupeJCL <dev@groupejcl.ca>
2018-07-30 15:21:10 -0500

    fix: query graphql not working in posts

--- wordcampreact/src/App.js
+++ wordcampreact/src/App.js
@@ -9,32 +9,38 @@ import { ApolloProvider } from "react-apollo";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./Components/Home";
 import Posts from "./Components/Posts";
+// import Dogs from "./Components/Dogs";
 // import Query from "./Components/Posts";
 
-const client ={
+const client = new ApolloClient({
   uri: "http://localhost/wordcamp2018/graphql"
-}
+   // uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <Router>
-        <div>
-          
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/posts">Posts</Link>
-            </li>
-          </ul>
-          <Route path="/" component={Home}/>
-          {/*<Route path="/posts" component={Posts}/>*/}
-          <Route path="/posts" component={Posts}/>
+          <div>
+            
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/posts">Posts</Link>
+              </li>
+              <li>
+                {/*<Link to="/dogs">dogs</Link>*/}
+              </li>
+            </ul>
+            <Route path="/" component={Home}/>
+            {/*<Route path="/dogs" component={Dogs}/>*/}
+            <Route path="/posts" component={Posts}/>
+            {/*<Route path="/posts" component={Posts}/>*/}
 
-        </div>
+          </div>
         </Router>
       </ApolloProvider>  
     );
--- wordcampreact/src/Components/Posts.js
+++ wordcampreact/src/Components/Posts.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
-import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import { Query } from "react-apollo";
 
 
 const Get_Posts = gql`
 	{
 		posts(first:2){
-         edges {
-           node{
-             title,
-             slug,
-             categories {
-               edges {
-                 node {
-                   name
-                 }
-               }
-             }
-           }
-         }
-       }
+         	edges {
+           		node{
+             		title,
+             		slug,
+             		categories {
+		               edges {
+		                 node {
+		                   name
+		                 }
+		               }
+             		}
+          		 }
+         	}
+       	}
 	}
-
 `;
 
 const Posts = () => (
@@ -29,9 +28,19 @@ const Posts = () => (
 	{
 		({ loading, error, data}) => {
 			if(loading) return "Loading...";
-			if(error) return "Error! ${error.message}"
+			if(error) return `Error! ${error.message}`;
+// console.log(data.posts);
+// console.log(data.posts.edges);
+			return( 
+				<div>
+					{
+						data.posts.edges.map(node => (
+							console.log(node.node)
+						))
 
-			return( console.log(data) )
+					}
+				</div>
+			);
 		}
 	}
 	</Query>
